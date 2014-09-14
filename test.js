415c6832f440d840a0988e82a23c3f0d0858a128
Arvo <ArvoGuo@gmail.com>
2014-09-14 21:49:59 +0800

    touchClear.html

--- test.js
+++ test.js
@@ -1,11 +1,24 @@
- var obj = document.getElementByIdx_x_x_x_x_x_x('id');
-  obj.addEventListener('touchmove', function(event) 
-  { // 如果这个元素的位置内只有一个手指的话
-        if (event.targetTouches.length == 1) 
-  {
-    var touch = event.targetTouches[0];
-      // 把元素放在手指所在的位置
-      obj.style.left = touch.pageX + 'px';
-         obj.style.top = touch.pageY + 'px';
-    }
-}, false);
+{
+	"appid ": "wx841a97238d9e17b2 ",
+	"img_url": "http: //i2.s2.dpfile.com/pc/promo/545fd924b641ac6036444f1ffc679865(440c272)/thumb.jpg",
+	"image": "http://i2.s2.dpfile.com/pc/promo/545fd924b641ac6036444f1ffc679865(440c272)/thumb.jpg",
+	"link": "http://event.dianping.com/activities/group/groupUserMemberInit?groupID=37937&groupType=1",
+	"url": "http://event.dianping.com/activities/group/groupUserMemberInit?groupID=37937&groupType=1",
+	"desc": "便所欢乐主题餐厅",
+	"timelineTitle": "我在【君威GS】真爱敢试团活动中开团成功了！\n快来加入我的团，大家一起免费吃喝玩乐吧！",
+	"title": "我在【君威GS】真爱敢试团活动中开团成功了！\n快来加入我的团，大家一起免费吃喝玩乐吧！",
+	"img_width": "200",
+	"img_height": "200"
+} {
+	"appid": "wx841a97238d9e17b2",
+	"img_url": "http://i3.s2.dpfile.com/pc/promo/783ce882158661730ec8ffc4f2e1dda0(440c272)/thumb.jpg",
+	"image": "http://i3.s2.dpfile.com/pc/promo/783ce882158661730ec8ffc4f2e1dda0(440c272)/thumb.jpg",
+	"link": "http://event.dianping.com/activities/group/groupSuccess?groupID=37935&groupType=2&name=%E7%BD%91%E6%98%93",
+	"url": "http://event.dianping.com/activities/group/groupSuccess?groupID=37935&groupType=2&name=%E7%BD%91%E6%98%93",
+	"desc": "MR.X先生密室",
+	"timelineTitle": "我在【君威GS】真爱敢试团活动中凑满团员啦！\n信心满满坐等中奖！你也赶快一起来免费吃喝玩乐吧！",
+	"title": "我在【君威GS】真爱敢试团活动中凑满团员啦！\n信心满满坐等中奖！你也赶快一起来免费吃喝玩乐吧！",
+	"img_width": "200",
+	"img_height": "200"
+}
+
